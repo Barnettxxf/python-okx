ce4888cf86f78511d269fe3913e3825182c23259
Barnett Xu <xuxiongfeng@yimian.com.cn>
2026-02-09 18:06:43 +0800

    add LIQUIDATION_ORDERS api

--- okx/utils.py
+++ okx/utils.py
@@ -49,7 +49,7 @@ def parse_params_to_str(params):
 
 
 def get_timestamp():
-    now = datetime.datetime.now(datetime.UTC)
+    now = datetime.datetime.utcnow()
     t = now.isoformat("T", "milliseconds")
     return t + "Z"
 
